28dea5c32fa67226070ad66b95078a1dd2005c89
TheDarkStrix <thedarkstrix@gmail.com>
2020-11-12 14:05:06 +0530

    configure app

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var port = process.env.PORT || 8080;
+var router = express.Router();
 
-app.get("/url", (req, res, next) => {
-  res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+// configure app to use bodyParser()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// al routers should be prefixed with api
+app.use("/api", router);
+
+//Initial router
+app.get("/", (req, res) => {
+  res.json({ message: "api working" });
 });
 
+//run the server
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
